0d664ddb1b8dbe5a4fd090f29f300b75536f8755
Thang Pham <phamducthang1234@gmail.com>
2021-01-14 17:31:29 +0900

    init vue

---
+++ web/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Transfer/>
+  </div>
+</template>
+
+<script>
+import Transfer from './components/Transfer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Transfer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ web/src/components/Transfer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>TBA</div>
+</template>
+
+<script>
+// Copyright (c) 2021 Curvegrid Inc.
+</script>
